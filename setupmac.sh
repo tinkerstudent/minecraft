7da19102664b12ff294a78f057b28e7847a55b72
tinkeracademy <ron@tinkeracademy.com>
2015-09-17 20:25:16 -0500

    bash shell mac

--- setupmac.sh
+++ setupmac.sh
@@ -2,24 +2,32 @@
 
 echo "Setting up minecraft forge on Mac"
 
-echo "Running ./gradlew setupDecompWorkspace"
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+echo "Running gradlew setupDecompWorkspace"
+
+cd $DIR
 
 ./gradlew setupDecompWorkspace
 
 if [ "$?" != "0" ]; then
-    echo "[Error] ./gradlew setupDecompWorkspace failed" 1>&2
+    echo "[Error] gradlew setupDecompWorkspace failed" 1>&2
+    sleep 10
     exit 1
 fi
 
-echo "Running ./gradlew eclipse"
+echo "Running gradlew eclipse"
 
 ./gradlew eclipse
 
 if [ "$?" != "0" ]; then
-    echo "[Error] ./gradlew eclipse failed" 1>&2
+    echo "[Error] gradlew eclipse failed" 1>&2
+    sleep 10
     exit 1
 fi
 
 echo "Setup Successful"
+
+read -p "Press any key to continue"
 
 
